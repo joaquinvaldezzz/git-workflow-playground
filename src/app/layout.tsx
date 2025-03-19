9cc27c002c898a07b7e6c9d8a4e3220c9447243a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-19 17:57:11 +0800

    chore: initialize Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react";
+
+import "./globals.css";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
