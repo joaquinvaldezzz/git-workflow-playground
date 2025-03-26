e8974869384fca4b2f9efd65fb58ad8ab0b6c10a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-26 10:08:40 +0800

    feat: add my name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Page() {
     <div className="container mx-auto px-4 pt-4">
       <ul className="list-inside list-disc">
         <li>John Joaquin A. Valdez</li>
+        <li>Claire Samson</li>
       </ul>
     </div>
   );
