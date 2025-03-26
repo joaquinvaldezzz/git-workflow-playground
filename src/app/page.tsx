ed3e6e334b771ba07642c9e5fee5ba61369204b9
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-03-26 10:13:59 +0800

    Add interns' names (#16)
    * feat: add my name
    
    * feat: add my name
    
    * feat: add my name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ export default function Page() {
     <div className="container mx-auto px-4 pt-4">
       <ul className="list-inside list-disc">
         <li>John Joaquin A. Valdez</li>
+        <li>Christal Sofia D. Malbago</li>
+        <li>Charlestone Maru</li>
+        <li>Ken Lorence M. Gara</li>
         <li>Claire Samson</li>
       </ul>
     </div>
